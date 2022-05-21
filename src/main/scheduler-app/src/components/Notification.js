ed92302af916dab996bb738bc354b228e4c8e912
Keresztes Beata <keresztesbeata00@yahoo.com>
2022-05-21 12:57:51 +0300

    react notification to onclose clalback

--- src/main/scheduler-app/src/components/Notification.js
+++ src/main/scheduler-app/src/components/Notification.js
@@ -4,19 +4,22 @@ import {Alert} from 'react-bootstrap'
 class Notification extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showNotification: true,
+        }
         this.hideNotification = this.hideNotification.bind(this);
     }
 
     hideNotification() {
         this.setState({
-            show: false
+            showNotification: false
         });
     }
 
     render() {
         return (
             <Alert dismissible={true} onClose={this.hideNotification}
-                   className={this.props.type} show={this.props.show}>
+                   className={this.props.type} show={this.props.show && this.state.showNotification}>
                 {this.props.message}
             </Alert>
         )
